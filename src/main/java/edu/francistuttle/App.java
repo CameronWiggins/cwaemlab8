31ab7d1e64b1b30db5aa9e1393a11ed2ae63bb52
CameronWiggins <cw1101046@student.francistuttle.edu>
2024-04-24 09:51:37 -0500

    finished parse method

--- src/main/java/edu/francistuttle/App.java
+++ src/main/java/edu/francistuttle/App.java
@@ -2,6 +2,11 @@ package edu.francistuttle;
 
 import java.util.ArrayList;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.*;
@@ -10,73 +15,98 @@ import java.io.*;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main(String[] args) throws Exception {
-      /* 
-        Document document = readXMLDocumentFromFile("C:\\Users\\cw1101046\\Desktop\\Github\\cwparse\\src\\main\\java\\edu\\francistuttle\\lab.xml");
-    
-        //Verify XML Content
-    
-        //Here comes the root node
-        Element root = document.getDocumentElement();
-        System.out.println(root.getNodeName());
-    
-        //Get all employees
-        NodeList nList = document.getElementsByTagName("computer");
-        System.out.println("============================");
-    
-        for (int temp = 0; temp < nList.getLength(); temp++) {
-          Node node = nList.item(temp);
-        
-          if (node.getNodeType() == Node.ELEMENT_NODE) {
-            //Print each employee's detail
-            Element eElement = (Element) node;
-            System.out.println("\nComputer id : " + eElement.getAttribute("id"));
-            System.out.println("Manufactor : " + eElement.getElementsByTagName("manufactor").item(0).getTextContent());
-            System.out.println("Year Purchased : " + eElement.getElementsByTagName("yearPurchased").item(0).getTextContent());
-            System.out.println("Processor : " + eElement.getElementsByTagName("processor").item(0).getTextContent());
+public class App {
+  public static void main(String[] args) throws Exception {
 
-            NodeList nList2 = eElement.getElementsByTagName("monitor");
+    Document document = readXMLDocumentFromFile(
+        "C:\\Users\\cw1101046\\Desktop\\Github\\cwaemlab8\\src\\main\\java\\edu\\francistuttle\\n" + //
+            "asa.xml");
 
-           for (int i = 0; i < nList2.getLength(); i++) {
-           Node childNode = nList2.item(i);
-            
-            if (childNode.getNodeType() == Node.ELEMENT_NODE) {
-                //Print each employee's detail
-                Element eElement2 = (Element) childNode;
-                System.out.println("\n\tMonitor id : " + eElement2.getAttribute("id"));
-                System.out.println("\tManufactor : " + eElement2.getElementsByTagName("manufactor").item(0).getTextContent());
-                System.out.println("\tResolution : " + eElement2.getElementsByTagName("resolution").item(0).getTextContent());
-            }
-            }
-          }
-        }
-      */
- 
-      Channel cs = new Channel();
+    // Verify XML Content
 
-      ArrayList<Item> lab1 = cs.parse("C:\\Users\\cw1101046\\Desktop\\Github\\cwparse\\src\\main\\java\\edu\\francistuttle\\lab.xml");
+    // Here comes the root node
+    Element root = document.getDocumentElement();
+    System.out.println(root.getNodeName());
 
-      for (Item c : lab1)
-      {
-        c.print();
+    // Get all employees
+    NodeList nList = document.getElementsByTagName("item");
+    System.out.println("============================");
+
+    for (int temp = 0; temp < nList.getLength(); temp++) {
+      Node node = nList.item(temp);
+
+      if (node.getNodeType() == Node.ELEMENT_NODE) {
+        // Print each employee's detail
+        Element eElement = (Element) node;
+        System.out.println("\nTitle : " + eElement.getElementsByTagName("title").item(0).getTextContent());
+        System.out.println("Link : " + eElement.getElementsByTagName("link").item(0).getTextContent());
+        System.out.println("Publish Date : " + eElement.getElementsByTagName("pubDate").item(0).getTextContent());
+
       }
     }
+  }
 
-    public static Document readXMLDocumentFromFile(String fileNameWithPath) throws Exception {
+  public static Document readXMLDocumentFromFile(String fileNameWithPath) throws Exception {
 
-        //Get Document Builder
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-    
-        //Build Document
-        Document document = builder.parse(new File(fileNameWithPath));
-    
-        //Normalize the XML Structure; It's just too important !!
-        document.getDocumentElement().normalize();
-    
-        return document;
+    // Get Document Builder
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+
+    // Build Document
+    Document document = builder.parse(new File(fileNameWithPath));
+
+    // Normalize the XML Structure; It's just too important !!
+    document.getDocumentElement().normalize();
+
+    return document;
+  }
+
+  public ArrayList<RSSItem> parseRssFeed(String url) {
+    try {
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = factory.newDocumentBuilder();
+
+      CloseableHttpClient client = HttpClients.createDefault();
+      HttpGet httpGet = new HttpGet(url);
+
+      try (CloseableHttpResponse response1 = client.execute(httpGet)) {
+        final HttpEntity entity = response1.getEntity();
+        if (entity != null) {
+
+          try (InputStream inputStream = entity.getContent()) {
+            Document document = builder.parse(inputStream);
+            Element root = document.getDocumentElement();
+            System.out.println(root.getNodeName());
+
+            // Get all employees
+            NodeList nList = document.getElementsByTagName("item");
+            ArrayList<RSSItem> rssList = new ArrayList<RSSItem>();
+            System.out.println("============================");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+              Node node = nList.item(temp);
+
+              if (node.getNodeType() == Node.ELEMENT_NODE) {
+                // Print each employee's detail
+                Element eElement = (Element) node;
+                // System.out.println("\nTitle : " + eElement.getElementsByTagName("title").item(0).getTextContent());
+                // System.out.println("Link : " + eElement.getElementsByTagName("link").item(0).getTextContent());
+                // System.out.println("Publish Date : " + eElement.getElementsByTagName("pubDate").item(0).getTextContent());
+                String title = eElement.getElementsByTagName("title").item(0).getTextContent();
+                String link = eElement.getElementsByTagName("link").item(0).getTextContent();
+                String pubDate = eElement.getElementsByTagName("pubDate").item(0).getTextContent();
+                RSSItem item = new RSSItem(title, link, "", "", pubDate);
+              }
+            }
+            return rssList;
+          }
+        }
+      } catch (Exception e) {
+        System.out.println("Hosed: " + e.toString());
+      }
+    } catch (Exception e) {
+      System.out.println("Error: " + e.toString());
     }
-    
+
+  }
 }
